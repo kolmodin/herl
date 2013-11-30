3e2b9901b6845660aa252987449d5afde35fada7
Lennart Kolmodin <kolmodin@gmail.com>
2013-12-01 01:04:22 +0400

    Add second main, to run the pingpong demo.

--- Main.hs
+++ Main.hs
@@ -6,4 +6,7 @@ import           ETerm
 import           Process
 
 main :: IO ()
-main = print =<< runBeam ["other.beam"] "fib_slow" [EInteger 21]
+main = print =<< runBeam ["other.beam"] "other" "fib_slow" [EInteger 21]
+
+main2 :: IO ()
+main2 = print =<< runBeam ["pingpong.beam"] "pingpong" "start" []
